9438a28cdf7b9d480bd9c16bbbf0c06c11724d15
Faqih Indra Lesmana <222112039@stis.ac.id>
2024-09-11 09:27:40 +0700

    Mantra telah selesai

--- dukapps/app.py
+++ dukapps/app.py
@@ -3,6 +3,10 @@ import os
 import numpy as np
 import joblib
 import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error
 
 app = Flask(__name__)
 
@@ -14,7 +18,7 @@ def predictedK():
     # Mengambil data JSON dari permintaan
     data = request.get_json()
     input_values = data.get('values')
-    
+
     if input_values is None:
         return jsonify({'error': 'Missing "values" in request data'}), 400
 
@@ -26,7 +30,7 @@ def predictedK():
 
         best_models = {
             'Random Forest': joblib.load(os.path.join(MODEL_PATH, 'Random Forest_model.pkl')),
-            'XGBoost': joblib.load(os.path.join(MODEL_PATH, 'XGBoost_model.pkl')),
+            'XGBoost': joblib.load(os.path.join(MODEL_PATH, 'XGBoost_model.pkl'))
         }
 
         # Transformasi input
@@ -49,29 +53,31 @@ def predictedK():
         return jsonify({'error': str(e)}), 500
 
 @app.route('/predictpengangguran', methods=['POST'])
-def predict_arimaxx():
+def predictedP():
     # Mengambil data JSON dari permintaan
     data = request.get_json()
-    input_values = data['values']
+    input_values = data.get('values')
+    
+    if input_values is None:
+        return jsonify({'error': 'Missing "values" in request data'}), 400
 
-    # Mengubah query menjadi array numpy
-    exog = np.array(input_values).reshape(1, -1)
-
-    # Melakukan prediksi
-    forecast = modelkemiskinan.predict(n_periods=1, exogenous=exog)
-
-    # Menangani berbagai kemungkinan output
-    if isinstance(forecast, pd.Series):
-        result = forecast.iloc[0]
-    elif isinstance(forecast, np.ndarray):
-        result = forecast[0]
-    elif isinstance(forecast, (float, int)):
-        result = forecast
-    else:
-        raise ValueError(f"Unexpected forecast type: {type(forecast)}")
-
-    return jsonify(result)
-
+    try:
+        num_features = len(input_values)  # Adjust this if you have a fixed number of features
+        input_df = pd.DataFrame([num_features], columns=['Lowongan Kerja'])  # Replace with actual feature names
+        
+        best_models = {
+            'XGBoost': joblib.load(os.path.join(MODEL_PATH, 'XGBoost_Pengangguran.pkl')),
+            'Random Forest': joblib.load(os.path.join(MODEL_PATH, 'Random Forest_Pengangguran.pkl'))
+        }
+        forecast_results = {}
+        for name, model in best_models.items():
+            forecast_value = model.predict(input_df)
+            forecast_results[name] = forecast_value.tolist()
+            
+        return jsonify(forecast_results)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/')
 def home():
